79d10177a066fc4b8439b03ed962682107498bf9
Max <maximizer38@gmail.com>
2022-11-18 11:54:02 +0700

    вторая задача

---
+++ 2_З/Program.cs
@@ -0,0 +1,39 @@
+﻿// a = 2, b = 3, c = 7
+// a = 44, b = 5, c = 78
+// a = 22, b = 3, c = 9
+
+Console.WriteLine("Напишите первое число");
+string str = Console.ReadLine();
+int a = int.Parse(str);
+Console.WriteLine("Напишите второе число");
+str = Console.ReadLine();
+int b = int.Parse(str);
+Console.WriteLine("Напишите третье число");
+str = Console.ReadLine();
+int c = int.Parse(str);
+int max = int.Parse(str);
+if(a > b)
+{
+    a = max;
+}
+else
+{
+    b = max;
+}
+if(b > c)
+{
+    b = max;
+}
+else
+{
+    c = max;
+}
+if(c > a)
+{
+    c = max;
+}
+else
+{
+    a = max;
+}
+Console.WriteLine("максимальное число " + max);
