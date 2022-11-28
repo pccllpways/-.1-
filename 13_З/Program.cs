02e4a04c3fdafc3bdb21e9705c3d56d6e7b1a6e5
Lola <maximizer38@gmail.com>
2022-11-28 15:30:29 +0700

    21_З

--- 13_З/Program.cs
+++ 13_З/Program.cs
@@ -7,53 +7,17 @@
 Console.WriteLine("Введите число: ");
 string str = Console.ReadLine();
 int number = int.Parse(str);
-int number1 = 100;
-int number2 = 1000;
-int number3 = 10000;
-int number4 = 100000;
-int number5 = 1000000;
-int number6 = 10000000;
-int number7 = 100000000;
-int number8 = 1000000000;
-if (number < number1)
+
+if (number >= 100)
 {
-    Console.WriteLine("Третьей цифры нет");
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int three = number % 10;
+    Console.WriteLine("Третья цифра числа: " + number);
 }
-else if (number < number2)
+else
 {
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number < number3)
-{
-    number = number % 100;
-    Console.WriteLine("Третья цифра числа: " + number / 10);
-}
-else if (number < number4)
-{
-    number = number / 100;
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number < number5)
-{
-    number = number / 1000;
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number < number6)
-{
-    number = number / 10000;
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number < number7)
-{
-    number = number / 100000;
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number < number8)
-{
-    number = number / 1000000;
-    Console.WriteLine("Третья цифра числа: " + number % 10);
-}
-else if (number > number8)
-{
-    Console.WriteLine("Величина числа превышена");
+    Console.WriteLine("Третьеё цифры нет");
 }
